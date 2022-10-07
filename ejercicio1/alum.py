50eaaac3d1b537d0e68b656fdbbc5b4a6e6b2616
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-10-07 15:46:01 +0200

    funcion calificacion

--- ejercicio1/alum.py
+++ ejercicio1/alum.py
@@ -2,3 +2,13 @@ class Alumno:
     def __init__(self,nombre,nota):
         self.nombre=nombre
         self.nota=nota
+    def __str__(self):
+        return f"el alumno {self.nombre} ha sido creado con exito"
+    def calif(self):
+        a = self.nota
+        if a>=5:
+            print("el alumno ha sido aprobado")
+        elif a < 5:
+            print("el alumno no ha aprobado")
+        else:
+            pass
