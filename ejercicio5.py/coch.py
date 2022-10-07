fcd7a5c9d19aa797d1f97482cfec7ed8a68c3a53
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-10-07 16:12:15 +0200

    clase moto

--- ejercicio5.py/coch.py
+++ ejercicio5.py/coch.py
@@ -22,5 +22,26 @@ class Camion(Vehiculo):
     def __str__(self):
         return Vehiculo.__str__(self) + ", {} kg".format(self.carga)
     
+class Motocicleta(Vehiculo):
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        Vehiculo.__init__(color,ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+        
+    def __str__(self):
+        return Vehiculo.__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
+
+    def set_velocidad(self,velocidad):
+        self.velocidad=velocidad
+    def get_velocidad(self):
+        return self.velocidad
+    
+    def set_cilindrada(self,cilindrada):
+        self.cilindrada=cilindrada
+    def get_cilindrada(self):
+        return self.cilindrada
+
+    
+    
         
         
