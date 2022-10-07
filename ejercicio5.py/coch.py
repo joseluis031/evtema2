f2944384003a373abd6456e9d28895de5e9e5798
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-10-07 15:55:43 +0200

    herencia coche

---
+++ ejercicio5.py/coch.py
@@ -0,0 +1,15 @@
+class Vehiculo():
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+    def __str__(self):
+        return "color {}, {} ruedas".format( self.color, self.ruedas )
+
+
+class Coche(Vehiculo):
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        Vehiculo.__init__(self, color, ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+    def __str__(self):
+        return Vehiculo.__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
